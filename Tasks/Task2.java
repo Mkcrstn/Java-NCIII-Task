f269907a7013fcb1838223816abd317cc2336946
Mkcrstn <maikacristina1995@gmail.com>
2025-08-06 13:49:54 +0000

    Task 2

---
+++ Tasks/Task2.java
@@ -0,0 +1,17 @@
+package Tasks;
+
+public class Task2 {
+     public static void main (String [] args){
+        byte zero=0;
+        int tertiwan = 31;
+        short ten = 10;
+        char[] letters={'H','w' ,'r'};
+        float tupointzero = 2.0f;
+        boolean isTrue=true;
+        String output2 ="ld"; 
+        
+        //rule: use "" to avoid math and print all the values as text.
+
+        System.out.println(""+ letters[0] + tertiwan + ten + " " + letters[1] + zero + letters[2] + output2 + " " +tupointzero +" " + isTrue );
+    }
+}
