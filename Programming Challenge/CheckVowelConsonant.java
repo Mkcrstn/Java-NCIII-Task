250e49e6847ad08618623723e1aeb66164e9e34d
Mkcrstn <maikacristina1995@gmail.com>
2025-08-06 12:45:39 +0000

    Vowel & Consonant

--- Programming Challenge/CheckVowelConsonant.java
+++ Programming Challenge/CheckVowelConsonant.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class CheckVowelConstant {
+public class CheckVowelConsonant {
 
     public static boolean isVowel(char c) {
         c  = Character.toLowerCase(c);
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ; 
     }
 
-    public static boolean isConstant(char c){
+    public static boolean isConsonant(char c){
         return Character.isLetter(c) && !isVowel(c);
        
     }
@@ -17,14 +17,14 @@ public class CheckVowelConstant {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
 
-        System.out.println("Enter a Character");
+        System.out.println("Enter a Character: ");
         char x = in.next().charAt(0);
 
         if(isVowel(x)){
             System.out.println(x + " is a Vowel");
         }
-        else if(isConstant(x)){
-            System.out.println(x + " is a Constant");
+        else if(isConsonant(x)){
+            System.out.println(x + " is a Consonant");
         }
         else
         System.out.println("Invalid input");
